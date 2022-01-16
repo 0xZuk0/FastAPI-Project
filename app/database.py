a1e2f8f1aa3d244bff779f4023e2c039fcdb90fb
0xZuk0 <cyber.antiru@gmail.com>
2022-01-16 20:06:24 +0530

    Fixing Bug 1

--- app/database.py
+++ app/database.py
@@ -5,7 +5,8 @@ from psycopg2.extras import RealDictCursor
 from psycopg2 import connect
 from time import sleep
 from .config import settings
-
+import matplotlib
+matplotlib.use('Agg')
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
